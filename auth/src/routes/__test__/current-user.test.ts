f11a6f887bb0e48db81a1d3598b119a9cec54c58
alfredoizjr <alfredo.alucar@gmail.com>
2021-05-17 11:23:48 -0400

    add currenUser test if singout

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,11 +1,12 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+
 describe('Testing api/users/currentuser', () => {
 
     test('Respond with details about current User', async () => {
 
-        const cookies =  await global.singin();
+        const cookies = await global.singin();
 
         const response = await request(app)
             .get('/api/users/currentuser')
@@ -16,6 +17,14 @@ describe('Testing api/users/currentuser', () => {
         expect(response.body.currentUser.email).toEqual('test@test.com');
     });
 
+    it('response with error if not authenticated', async () => {
+        const response = await request(app)
+            .get('/api/users/currentuser')
+            .send()
+            .expect(401);
+
+        expect(response.body.errors[0].message).toEqual('Not authorized');
+    });
 
 
 })
